799923e19cf6806464e7adf1e65c520782e1065c
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-06-07 22:21:11 +0000

    started work on the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import youtubeshorts from '../images/youtubeshorts.jpg'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+        <div className='sidebarinner'>
+            <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+            <h3>Home</h3>
+        </div>
+        <div className='sidebarinner'>
+            <img src={youtubeshorts} width={35} height={27}/>
+            <h3>Shorts</h3>
+        </div>
+    </div>
   )
 }
 
