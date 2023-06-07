1f2765b3fa75d1ea0ffa1b5392b3419599d0bc30
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-06-07 19:30:01 +0000

    completed work on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,28 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
+import { faVideoCamera } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
     <div className='nav'>
         <div className='innernav'>
             <div className='namesection'>
-                <FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faYoutube} size='2x' color='red'></FontAwesomeIcon>
                 <h3>YouTube</h3>
             </div>
             <div className='searchsection'>
                 <input type='text' placeholder='Search' size={60}/>
                 <button type='submit'><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></button>
+                <FontAwesomeIcon icon={faMicrophone}></FontAwesomeIcon>
+            </div>
+            <div className='extras'>
+                <FontAwesomeIcon icon={faVideoCamera}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faBell} size='xl'></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faUserCircle} size='2x' color='lightblue'></FontAwesomeIcon>
             </div>
         </div>
     </div>
